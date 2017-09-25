e71a4cdc02a50bad667612ddd6391c4be1bdd2ee
Greg Borota <borota@gmail.com>
2017-09-25 02:58:02 -0500

    more unit tests

--- test/index_test.js
+++ test/index_test.js
@@ -9,6 +9,11 @@ describe('Hebrew', () => {
       const word = 'התשע"ד';
       test.strictEqual(number, word, 'getNumber_generic');
     });
+    it('Another general case', () => {
+      const number = mapper.getNumber(774);
+      const word = 'תשע"ד';
+      test.strictEqual(number, word, 'getNumber_generic');
+    });
     it('Truncate to Limit', () => {
       const number = mapper.getNumber(5774, 3);
       const word = 'תשע"ד';
@@ -18,6 +23,23 @@ describe('Hebrew', () => {
       const number = mapper.getNumber(5774, 7);
       const word = 'התשע"ד';
       test.strictEqual(number, word, 'getNumber_generic');
+    });
+  });
+  describe('Letters => Number', () => {
+    it('General case', () => {
+      const number = mapper.getNumber('התשע"ד', true);
+      const word = 5774;
+      test.strictEqual(number, word, 'getNumber_generic');
+    });
+    it('Truncate to Limit', () => {
+      const number = mapper.getNumber('תשע"ד', true);
+      const word = 774;
+      test.strictEqual(number, word, 'getNumber_generic shorter');
+    });
+    it('Limit but un-truncated', () => {
+      const number = mapper.getNumber('התשע"ד', false);
+      const word = 779;
+      test.strictEqual(number, word, 'getNumber_generic un-ordered');
     });
   });
 });
